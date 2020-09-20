01d0786cb5ab8d8cc806d0fcfcf141748acbdea7
Макар Зарчуков <makzarchukov@gmail.com>
2020-09-20 20:34:51 +0500

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("hello world!");
+console.log("Denis!");
+console.log("Wellcome ochinchin");
+console.log("Nikita!");
+console.log('Vladimir');
+console.log("Vika");
+console.log("Довод");
