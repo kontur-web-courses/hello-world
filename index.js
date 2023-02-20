b99ec090168de4d5c4c09970d9f8505ad4ce3003
Matvey Lipilin <matvey.lipilin1@gmail.com>
2023-02-20 18:06:10 +0500

    feat!: add magic feature

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello world!");
+function randomizeString(str) {
+    let arr = str.split('');
+  
+    for (let i = 0; i < arr.length; i++) {
+      let randomIndex = Math.floor(Math.random() * arr.length);
+      let temp = arr[i];
+      arr[i] = arr[randomIndex];
+      arr[randomIndex] = temp;
+    }
+  
+    return arr.join('');
+}
+
+hashCode = function(s) {
+return s.split("").reduce(function(a, b) {
+    a = ((a << 5) - a) + b.charCodeAt(0);
+    return a & a;
+}, 0);
+}
+
+const input = 'sirileNKggl '
+const magicConstant = -1039686423;
+
+while (true) {
+    const output = randomizeString(input)
+    const hash = hashCode(output)
+    if (hash === magicConstant) {
+        console.log("Oh no...")
+        break
+    }
+    console.log(`"${output}" is not quite right`)
+}
+  
