a810710ed8d30e3d32ed08eaeb3bb6ec16339230
Tarakanoed <svitch1337@gmail.com>
2024-02-20 18:54:01 +0500

    Task Done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 function solveTheWorld(){
     console.log('42')
 }
 console.log("hello world!");
 solveTheWorld();
+=======
+console.log("hello world! pupupup");
+>>>>>>> Stashed changes
